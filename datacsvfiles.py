f5065484ae9065e0e30733a427dfa759d8b34604
sushmasatya <sushmasatyapasupuleti@gmail.com>
2026-06-07 03:41:08 +0530

    Remove unnecessary files from repository

--- datacsvfiles.py
+++
@@ -1,7 +0,0 @@
-import json
-import pandas as pd
-
-path = r"C:\Users\sushma\IdeaProjects\untitled4"
-df = pd.DataFrame(json.load(open(f"{path}/customer_data.json", encoding="utf-8")))
-df.to_csv(f"{path}/customer_data.csv", index=False, encoding="utf-8")
-print(df["email"].str.len().max())
